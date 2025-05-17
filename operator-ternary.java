63664314b362f6d1dec4a28289e1b8876133f270
Hanif Luqmanul Hakim <hanifluqmanulhakim20@gmail.com>
2025-05-17 11:18:56 +0700

    Create operator-ternary.java

---
+++ operator-ternary.java
@@ -0,0 +1,19 @@
+package com.mycompany.operatorternary;
+
+/**
+ * Demonstrasi Operator Ternary dalam Java
+ * @author luqmanaru
+ */
+public class OperatorTernary {
+    public static void main(String[] args) {
+        boolean suka = false;
+        String jawaban;
+        
+        // Menggunakan operator ternary
+        jawaban = suka ? "iya" : "tidak";
+        
+        // Menampilkan jawaban
+        System.out.println(jawaban);
+    }
+}
+Perbaikan yang dilakukan:
